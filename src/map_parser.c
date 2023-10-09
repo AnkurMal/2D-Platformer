f66fabf5959614416c43b24e545ccdafcaa29cca
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2023-10-09 23:42:19 +0530

    Update map_parser.c

--- src/map_parser.c
+++ src/map_parser.c
@@ -5,18 +5,17 @@
 #define XOR_KEY 0x5A
 
 char *data;
-int start_pos = 0, level = 0;
+int start_pos = 0;
 
 void LoadMapData(Map *map)
 {
-    level++;
-    map->level = level;
-	map->row= 1;
+    map->level++;
+    map->row = 1;
     map->column = 0;
 
-    if(level==1)
+    if(map->level==1)
     {
-        data = LoadFileText("../resources/level-map/map.bin");
+        data = LoadFileText("level-map/map.bin");
         int data_len = TextLength(data);
         for(int i=0; i<data_len; i++)
             data[i] ^= XOR_KEY;
@@ -36,7 +35,7 @@ void LoadMapData(Map *map)
 			map->row++;
 		}
     }
-    if(level==1)
+    if(map->level==1)
         map->data = malloc(map->row*map->column*sizeof(char));
     else
         map->data = realloc(map->data, map->row*map->column*sizeof(char));
@@ -54,5 +53,5 @@ void UnloadMapData(Map *map)
     if(map->data!=NULL)
         free(map->data);
     else
-        fprintf(stderr, "ERROR: Could not load level due to insufficient storage. Terminating program.");
+        fprintf(stderr, "ERROR: Could not load level due to insufficient storage. Terminating application.");
 }
